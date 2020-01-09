603fa24be16e07905df2aa90842b7e65bea1d06a
Noah231515 <44912201+Noah231515@users.noreply.github.com>
2020-01-08 19:28:25 -0500

    Update Exercises.js

--- Exercises.js
+++ Exercises.js
@@ -37,14 +37,17 @@ function Exercise2(word){
 inputs_1 = [[5,3,2,8,1,4], [6,3,2,7,5,0]];
 inputs_2 = ["dog", "Fishing", "have", "Midddle", "C", ""]
 expected_output_1 = [[1,3,2,8,5,4], [6,3,2,5,7,0]];
-expected_output_2 = ["o", "h", "have", "Middle", "C", ""];
+expected_output_2 = ["o", "h", "av", "dd", "C", ""];
 
 for (let i = 0; i < inputs_1.length; i++){
     expected_1 = JSON.stringify(expected_output_1[i]); 
     output_1 = JSON.stringify(Exercise1(inputs_1[i]));
 
-    console.assert(expected_1 == output_1, `Test Case failed on Exercise 1, test ${i}!`);
-    console.assert(expected_output_2[i] == Exercise2(inputs_2[i]), `Test Case failed on Exercise 2, test ${i}!`);    
+    console.assert(expected_1 == output_1, `Test Case failed on Exercise 1, test ${i}!`);    
 }
 
+for (let j = 0; j < inputs_2.length; j++) {
+     console.assert(expected_output_2[j] == Exercise2(inputs_2[j]), `Test Case failed on Exercise 2, test ${j}!`);
+}
+   
 
